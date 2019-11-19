829ae5cbc645efe2f8edfa64584e789bf6fa36f4
Nikola Spasic <nikola@spasic.dev>
2019-11-19 22:39:44 +0100

    10.1 MarkdownLogger, bind,...

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,21 +5,28 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $markdownLogger;
     }
 
     public function parse(string $sorce): string
     {
+        if (stripos($sorce, 'bacon') !== false) {
+            $this->logger->info('They talking about bacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($sorce));
         if(!$item->isHit()) {
             $item->set($this->markdown->transform($sorce));
